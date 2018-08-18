3e1ab413d9286ae2e2b050a0f35e4765f8c6b6df
liufenglin <liufenglin@baidu.com>
2018-08-18 23:07:31 +0800

    init

---
+++ js/head.js
@@ -0,0 +1,5 @@
+document.write('<link rel="script" href="../plugins/js/bootstrap.min.js">');
+document.write('<link rel="script" href="../plugins/js/bootstrap.bundle.min.js">');
+document.write('<link rel="stylesheet" href="../plugins/css/bootstrap.min.css">');
+document.write('<link rel="stylesheet" href="../plugins/css/bootstrap-grid.min.css">');
+document.write('<link rel="stylesheet" href="../plugins/css/bootstrap-reboot.min.css">');
